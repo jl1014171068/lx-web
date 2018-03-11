0e03f6fad62253023ceab723a812c57cf8c42a2e
江龙 <1146937621@qq.com>
2018-03-11 13:11:51 +0800

    vee-validate自定义属性/消息、自定义全局属性/消息、自定义规则完成 ，存储

--- pages/lender/add.vue
+++ pages/lender/add.vue
@@ -3,12 +3,18 @@
     <titleField>
       <h1 slot='title' class="leg-text">开户</h1>
       <el-form ref="form" :inline="true" :model="form" label-position="right" label-width="80px" slot='con'>
-
-        <el-form-item label="资方编码" for="email" :class="{ 'vee-control': true }">
-          <el-input v-model="form.name" v-validate="'required|email'" :class="{'input': true, 'is-danger': errors.has('email') }" name="email" type="text" placeholder="Email"></el-input>
-          <span v-show="errors.has('email')" class="help is-danger">{{ errors.first('email') }}</span>
+        <!--  <el-form-item label="活动名称" for="email" :class="{ 'vee-control': true }" >
+    <el-input v-model="form.name" v-validate="'required|email'" :class="{'input': true, 'is-danger': errors.has('email') }" name="email" type="text" placeholder="Email"></el-input>
+    <span v-show="errors.has('email')" class="help is-danger">{{ errors.first('email') }}</span>
+  </el-form-item> -->
+        <el-form-item label="资方编码" for="lender.code" :class="{ 'vee-control': true }">
+          <el-input v-model="form.code" v-validate="'required|sss'" :class="{'input': true, 'is-danger': errors.has('lender.code') }" name="lender.code" type="text" placeholder="资方编码"></el-input>
+          <span v-show="errors.has('lender.code')" class="help is-danger">{{ errors.first('lender.code') }}</span>
         </el-form-item>
-
+        <p>
+          <input v-validate="'required|email'" name="email" type="text" placeholder="Email">
+          <span v-show="errors.has('email')">{{ errors.first('email') }}</span>
+        </p>
         <el-form-item>
           <el-button type="primary" @click="onSubmit">立即创建</el-button>
           <el-button>取消</el-button>
@@ -23,13 +29,32 @@
 </style>
 <script>
 import titleField from '~/components/common/title-field/title-field'
-// import {Validator} from 'vee-validate';
+import { Validator } from 'vee-validate'
+
+
+const dictionary = {
+  zh_CN: {
+    messages: {
+      lender: {
+        code: () => 'ssss',
+      }
+    },
+    attributes: {
+      lender: {
+        code: '资方编码'
+      }
+    }
+  }
+};
+
+Validator.localize(dictionary);
+
 
 export default {
   data() {
     return {
       form: {
-        name: '',
+        code: '',
         region: '',
         email: '',
         date1: '',
--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -1,24 +1,72 @@
-
-
-
 import Vue from 'vue'
-import VeeValidate from 'vee-validate';
-import fr from 'vee-validate/dist/locale/zh_CN';
+import VeeValidate,{Validator} from 'vee-validate';
+import zh_CN from 'vee-validate/dist/locale/zh_CN';
 import VueI18n from 'vue-i18n';
 
 Vue.use(VueI18n);
 
 const i18n = new VueI18n({
-  locale: 'fr'
+  locale: 'zh_CN'
 });
 
 Vue.use(VeeValidate, {
   i18n,
   fieldsBagName: 'field',
   dictionary: {
-    fr
+    zh_CN: {
+      messages: {
+        email: () => '请输入正确的邮箱格式，哼哼哼哼哼',
+        required: (field) => "请输入" + field
+      },
+      attributes: {
+        // email:'邮箱有毒吗',
+        password: '密码',
+        name: '账号',
+        phone: '手机',
+        lender: {
+          code: '资方编码'
+        }
+      }
+    }
   }
 });
+
+
+
+// const sss = {
+//   getMessage(field, params, data) {
+//       return (data && data.message) || 'Something went wrong';
+//   },
+//   validate(value) {
+//     return new Promise(resolve => {
+//       resolve({
+//         valid: value === 'trigger' ? false : !! value,
+//         data: value !== 'trigger' ? undefined : { message: 'Not this value' }
+//       });
+//     });
+//   }
+// };
+// Validator.extend('sss', {
+//   getMessage: {
+//     // zh_CN: field => field + '必须是11位手机号码',
+//     return (data && data.message) || 'Something went wrong';
+//   },
+//   validate(value) {
+//     return new Promise(resolve => {
+//       resolve({
+//         valid: value === 'trigger' ? false : !! value,
+//         data: value !== 'trigger' ? undefined : { message: 'Not this value' }
+//       });
+//     });
+//   }
+// });
+
+Validator.extend('sss', {
+  getMessage: field =>  field + '必须是一个手机号.',
+  validate: value =>  {return value.length == 11 && /^((13|14|15|17|18)[0-9]{1}\d{8})$/.test(value)}
+});
+
+// let instance = new Validator({ trueField: 'truthy' });
 
 
 // const config = {
